0d48c6775f10a7278ad796d766ac5152103340d7
AkhileshNS <nsakhilesh02@gmail.com>
2018-10-03 00:37:07 +0530

    Added Image Config to Config file

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,20 @@
 
-const importProfiles = ['black_and_white','main','brandcampaign','shirtflirt'];
+const importProfiles = [
+    { 
+        name: 'black_and_white',
+        conf: ['1-n']
+    },{ 
+        name: 'main',
+        conf: ['1-n'] 
+    },{ 
+        name: 'brandcampaign',
+        conf: ['1-n'] 
+    },{ 
+        name: 'shirtflirt',
+        conf: ['1-n']
+    }
+];
+
 const exportProfiles = {};
 
 const displayProfiles = ['shirtflirtdemo','portfoliodemo','clients','category'];
@@ -8,7 +23,7 @@ const displayValues = [{
     no: [0,1,2]
 },{
     name: ['main','main','main','main','main','main','main','main','main'], 
-    no: [0,7,23,38,46,60,69,83,98]
+    no: [7,23,38,46,60,69,83,98]
 },{
     name: ['brandcampaign','brandcampaign','brandcampaign'], 
     no: [5,15,23]
@@ -19,22 +34,34 @@ const displayValues = [{
 
 for (let profile of importProfiles) {
 
-    exportProfiles[profile] = {
+    exportProfiles[profile.name] = {
         thumbnails: [],
         src: []
     };
 
-    for (let j=1;j<=999;j++) {
-        let pro = profile;
-        if (profile==='main') {
-            pro = 'portfolio';
+    for (let conf of profile.conf) {
+
+        let vals,start,end;
+
+        vals = conf.split("-");
+        start = parseInt(vals[0], 10);
+        end = (vals[1]==='n') ? 999 : parseInt(vals[1], 10);
+
+        console.log(start + '-' + end);
+
+        for (let j=start;j<=end;j++) {
+            let pro = profile.name;
+            if (profile.name==='main') {
+                pro = 'portfolio';
+            }
+            try {
+                exportProfiles[profile.name].thumbnails.push(require(`./assets/thumbnails/${pro}/${pro}_${j}.jpg`));
+                exportProfiles[profile.name].src.push(`public/${pro}/${pro}_${j}.jpg`);
+            } catch(error) {
+                break;
+            }
         }
-        try {
-            exportProfiles[profile].thumbnails.push(require(`./assets/thumbnails/${pro}/${pro}_${j}.jpg`));
-            exportProfiles[profile].src.push(`public/${pro}/${pro}_${j}.jpg`);
-        } catch(error) {
-            break;
-        }
+
     }
 
 }
