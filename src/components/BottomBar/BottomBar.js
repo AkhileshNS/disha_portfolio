293d7ab280f7997c2a9ff1c72091512b6000ac14
AkhileshNS <nsakhilesh02@gmail.com>
2018-10-04 22:13:17 +0530

    Completed Website - Content

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -1,37 +1,18 @@
 //External Libraries
 import React, {Component} from 'react';
-import Radium from 'radium';
-import {withRouter} from 'react-router-dom';
 
 //Internal Libraries
 import InstaImg from '../../assets/insta_btn.svg';
-
-let BottomBarStyle = {
-    width: '100%',
-    backgroundColor: '#F0F0F0'
-};
-
-let InstaBtnStyle = {
-    width: '100%',
-    height: '30px',
-    marginTop: '7px',
-    marginBottom: '4px',
-    cursor: 'pointer',
-    ':hover': {
-        height: '32px',
-        marginTop: '6px',
-        marginBottom: '3px',
-        transform: 'rotate(360deg)',
-        transition: 'all 0.5s ease'
-    }
-};
+import './BottomBar.css';
 
 class BottomBar extends Component {
     render() {
-        return <div style={BottomBarStyle}>
-            <img src={InstaImg} alt="Instagram" style={InstaBtnStyle} onClick={() => window.open('https://www.instagram.com/dishakhanted/?hl=en')}/>
+        return <div className='BottomBar'>
+            <p className='BottomBartext'>DishaKhanted</p>
+            <img src={InstaImg} alt="Instagram" className='BottomBarimg' onClick={() => window.open('https://www.instagram.com/dishakhanted/?hl=en')}/>
+            <p className='BottomBartext'>Copyright {new Date().getFullYear()}</p>
         </div>;
     }
 }
 
-export default withRouter(Radium(BottomBar));
+export default BottomBar;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,14 +4,14 @@ import React from 'react';
 
 // Internal Libraries
 import './About.css';
-import dummy from '../../assets/dummy_img.jpg';
+import selfie from '../../assets/Disha_pic.png';
 
 let cssClassName = 'About';
 
 const About = (props) => {
     return <div className={cssClassName}>
         <div>
-            <img src={dummy} alt="Unavailable" />
+            <img src={selfie} alt="Unavailable" />
             <p>Disha Khanted</p>
             <span>Photographer Disha Khanted spent the last couple of years photographing landscapes and people, when she found that her passion lies in portrait and fashion photography. It all began when she simply grabbed the camera lying in her home, and ventured out to shoot anything and everything that could be captured through a camera lens.</span>
             <span>Starting off with small count of followers, the number has risen exponentially over a span of 2 years. Not only does she play the role of a photographer, but also the roles of an editor and a stylist. She is also self taught in the techniques of lighting, angles and settings.</span>
