107063c38113cddfb53552d0c0f416495de1a87c
dishakhanted <43805337+dishakhanted@users.noreply.github.com>
2018-10-06 12:48:37 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -5,7 +5,7 @@ const importProfiles = [
         conf: [1]
     },{ 
         name: 'main',
-        conf: [1,2,3,4,5,6,7,8,9,10,11] 
+        conf: [1,2,3,4,5,6,7,8,9,10,11,12] 
     },{ 
         name: 'brandcampaign',
         conf: [1] 
